ae5c1ebc114c5f502c60af522b3cedf9e8502b2e
Michael Horwitz <michael@horwitz.co.za>
2024-06-05 03:45:41 +0200

    Glass rendering

--- sceneObject.cpp
+++ sceneObject.cpp
@@ -8,9 +8,14 @@ constexpr vec4 blue = vec4(55 / 255.0, 90 / 255.0, 128 / 255.0, 1);
 constexpr vec4 black = vec4(5 / 255.0, 5 / 255.0, 5 / 255.0, 1);
 constexpr vec4 white = vec4(240 / 255.0, 240 / 255.0, 240 / 255.0, 1);
 constexpr vec4 offWhite = vec4(220 / 255.0, 220 / 255.0, 220 / 255.0, 1);
+constexpr vec4 offOffWhite = vec4(200 / 255.0, 200 / 255.0, 200 / 255.0, 1);
 constexpr vec4 floorgrey = vec4(60 / 255.0, 55 / 255.0, 55 / 255.0, 1);
+constexpr vec4 wallgrey = vec4(85 / 255.0, 85 / 255.0, 85 / 255.0, 1);
+constexpr vec4 wallBlack = vec4(15 / 255.0, 15 / 255.0, 15 / 255.0, 1);
 constexpr vec4 yellow = vec4(200 / 255.0, 200 / 255.0, 50 / 255.0, 1);
 constexpr vec4 brown = vec4(71 / 255.0, 58 / 255.0, 48 / 255.0, 1);
+constexpr vec4 glassBlue = vec4(124 / 255.0, 183 / 255.0, 194 / 255.0, 0.5);
+
 
 void SceneObject::applyMatrix(mat4x4 m)
 {
@@ -130,7 +135,6 @@ std::string SceneObject::printColor()
     }
     return ret;
 }
-
 std::string SceneObject::printVertices()
 {
     int n = numPoints();
@@ -145,7 +149,6 @@ std::string SceneObject::printVertices()
     }
     return ret;
 }
-
 int SceneObject::numVertices()
 {
     int count = 0;
@@ -184,20 +187,33 @@ void SceneObject::addFloorsAndWalls()
         cts(50, 16, 1.5),
         offWhite));
     this->children.push_back(new Rectangle(
-    cts(50, 15, 3),
-    cts(0, 15, 3),
-    cts(0, 18, 14),
-    cts(50, 18, 14),
-    white));
-    // this->children.push_back(new OpenCuboid(
-    //     cts(50, 18, )
-    // ))
+        cts(50, 15, 3),
+        cts(0, 15, 3),
+        cts(0, 18, 14),
+        cts(50, 18, 14),
+        white));
+    this->children.push_back(new OpenCuboid(
+        cts(50, 18, 15),
+        cts(0, 18, 15),
+        cts(0, 18, 14),
+        cts(50, 18, 14),
+        cts(50, 20, 15),
+        cts(0, 20, 15),
+        cts(0, 20, 14),
+        cts(50, 20, 14),
+        offWhite));
     this->children.push_back(new Rectangle(
-    cts(50, -1, 1.5),
-    cts(0, -1, 1.5),
-    cts(0, 0, 0),
-    cts(50, 0, 0),
-    white));
+        cts(50, 18, 15),
+        cts(0, 18, 15),
+        cts(0, 20, 20),
+        cts(50, 20, 20),
+        white));
+    this->children.push_back(new Rectangle(
+        cts(50, -1, 1.5),
+        cts(0, -1, 1.5),
+        cts(0, 0, 0),
+        cts(50, 0, 0),
+        white));
     this->children.push_back(new Rectangle(
         cts(50, 0, 3),
         cts(0, 0, 3),
@@ -205,11 +221,42 @@ void SceneObject::addFloorsAndWalls()
         cts(50, -1, 1.5),
         offWhite));
     this->children.push_back(new Rectangle(
-    cts(50, 0, 3),
-    cts(0, 0, 3),
-    cts(0, -3, 14),
-    cts(50, -3, 14),
-    white));
+        cts(50, 0, 3),
+        cts(0, 0, 3),
+        cts(0, -3, 14),
+        cts(50, -3, 14),
+        white));
+    this->children.push_back(new OpenCuboid(
+        cts(50, -3, 15),
+        cts(0, -3, 15),
+        cts(0, -3, 14),
+        cts(50, -3, 14),
+        cts(50, -5, 15),
+        cts(0, -5, 15),
+        cts(0, -5, 14),
+        cts(50, -5, 14),
+        offWhite));
+    this->children.push_back(new Rectangle(
+        cts(50, -3, 15),
+        cts(0, -3, 15),
+        cts(0, -5, 20),
+        cts(50, -5, 20),
+        white));
+    this->children.push_back(new Rectangle(
+        cts(50, 20, 20),
+        cts(50, -5, 0),
+        wallgrey
+    ));
+    this->children.push_back(new Cuboid(
+        cts(50, 11.5, 20),
+        cts(48.5,3.5,0),
+        wallBlack
+    ));
+    this->children.push_back(new Rectangle(
+        cts(0.01, -5, 20),
+        cts(0, 20, 0),
+        glassBlue
+    ));
 }
 void SceneObject::addPanels()
 {
@@ -235,7 +282,6 @@ void SceneObject::addPanels()
         cts(36.25, 14, 3),
         white));
 }
-
 void SceneObject::addChairs()
 {
     for (int i = 0; i < 5; ++i)
@@ -320,7 +366,6 @@ void SceneObject::addChairs()
     //         TubChair::Facing::LEFT));
     // }
 }
-
 void SceneObject::addTables()
 {
     this->children.push_back(new BigTable(
@@ -328,19 +373,17 @@ void SceneObject::addTables()
     this->children.push_back(new BigTable(
         vec2(25, 12.0)));
 }
-
 SceneObject *SceneObject::makeBasicScene()
 {
     auto *so = new SceneObject();
-    so->addFloorsAndWalls();
     so->addPanels();
     so->addChairs();
     so->addTables();
-mat4 rotationMatrix = glm::rotate(mat4(1.0f), glm::radians(90.0f), vec3(1.0f, 0.0f, 0.0f));
-so->applyMatrix(transpose(rotationMatrix));
+    so->addFloorsAndWalls();
+    mat4 rotationMatrix = glm::rotate(mat4(1.0f), glm::radians(90.0f), vec3(1.0f, 0.0f, 0.0f));
+    so->applyMatrix(transpose(rotationMatrix));
     return so;
 }
-
 float SceneObject::cts(float x)
 {
     return x / 50 * 2;
@@ -517,10 +560,11 @@ Cuboid::Cuboid(vec3 fur, vec3 ful, vec3 fll, vec3 flr, vec3 bur, vec3 bul, vec3 
     }
 }
 
-OpenCuboid::OpenCuboid(vec3 fur, vec3 ful, vec3 fll, vec3 flr, vec3 bur, vec3 bul, vec3 bll, vec3 blr, vec4 color) {
+OpenCuboid::OpenCuboid(vec3 fur, vec3 ful, vec3 fll, vec3 flr, vec3 bur, vec3 bul, vec3 bll, vec3 blr, vec4 color)
+{
     children = std::vector<SceneObject *>();
-    //children.push_back(new Rectangle(fur, ful, fll, flr, color));
-    children.push_back(new Rectangle(bur, bul, bll, blr, color));
+    // children.push_back(new Rectangle(fur, ful, fll, flr, color));
+    children.push_back(new Rectangle(bur, bul, bll, blr, offOffWhite));
     children.push_back(new Rectangle(fur, bur, blr, flr, color));
     children.push_back(new Rectangle(ful, bul, bll, fll, color));
     children.push_back(new Rectangle(fur, bur, bul, ful, color));
